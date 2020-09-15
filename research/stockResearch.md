c8e6649ad40b2cd15594430d6809322649e71047
Christian Lussier <lussierc@allegheny.edu>
2020-09-15 00:50:06 -0400

    Implement basic scrape_article() function

--- research/stockResearch.md
+++ research/stockResearch.md
@@ -1,3 +1,5 @@
 # Stock Research
 
 General research about the stock market, what causes swings (in news and otherwise), and what techniques are used to predict it.
+
+## Research Points
--- src/article_scraper.py
+++ src/article_scraper.py
@@ -1,1 +1,14 @@
 """Web scrapes articles to be used in text mining analysis."""
+
+from newspaper import Article
+# NOTE: pip install newspaper3k
+
+link = input("* Enter Link: ")
+
+def scrape_article(link):
+    article = Article(link)
+    article.download()
+    article.parse()
+
+    print("Article Text:")
+    print(article.text)
