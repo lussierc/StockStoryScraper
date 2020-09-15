121bde9460fa5867da4d9e8f70adc358cd36b9ea
Christian Lussier <lussierc@allegheny.edu>
2020-09-15 01:03:28 -0400

    Add useful comments to article_scraper.py

--- src/article_scraper.py
+++ src/article_scraper.py
@@ -3,6 +3,10 @@
 from newspaper import Article
 # NOTE: pip install newspaper3k
 
+# TODO: Find a way to search specific websites for Articles
+# TODO: Find way to search for specific articles (regarding) specific companies, then download these Articles
+# TODO: ^ Eventually automate this process
+
 def main():
     link = input("* Enter Link: ")
     scrape_article(link)
