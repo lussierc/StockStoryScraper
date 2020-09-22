43eaf2da3cfec3ee5c4bba70f2a91134c5a18d15
Christian Lussier <lussierc@allegheny.edu>
2020-09-22 13:05:11 -0400

    Improve search_scraper.py formatting

--- src/search_scraper.py
+++ src/search_scraper.py
@@ -1,28 +1,24 @@
-# from googlesearch import search
-# #pip3 install google
-#
-# for url in search('IDEX', tld='com', stop=1):
-#     print(url)
+"""Performs web scraping on Google News web searches."""
 
 from GoogleNews import GoogleNews
-#pip3 install GoogleNews
+# pip3 install GoogleNews
 
-googlenews = GoogleNews()
+def main():
+    """Google News Web Scraping."""
+    # initialize google news:
+    googlenews = GoogleNews()
+    googlenews = GoogleNews(lang='en')
+    googlenews = GoogleNews(period='d')
+    googlenews = GoogleNews(encode='utf-8')
 
-googlenews = GoogleNews(lang='en')
+    # set search query parameters:
+    googlenews = GoogleNews(start='06/12/2020',end='09/20/2020')
+    googlenews.search('IDEX')
+    googlenews.getpage(1)
 
-googlenews = GoogleNews(period='d')
+    # print the results:
+    print(googlenews.result())  # prints all info
+    print("\n\n\n")
+    print(googlenews.gettext())  # prints titles
 
-googlenews = GoogleNews(start='06/12/2020',end='09/20/2020')
-
-googlenews = GoogleNews(encode='utf-8')
-
-googlenews.search('IDEX')
-
-googlenews.getpage(1)
-
-print(googlenews.result()) # prints all info
-
-print("\n\n\n")
-
-print(googlenews.gettext()) # prints titles
+main()
