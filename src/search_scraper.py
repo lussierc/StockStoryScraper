50b01f3199482e7db32da3e1f86fac2fc812f429
Christian Lussier <lussierc@allegheny.edu>
2020-09-23 18:50:38 -0400

    Add function docstrings and other minor improvements

--- src/search_scraper.py
+++ src/search_scraper.py
@@ -7,12 +7,13 @@ from newspaper import Article
 
 
 def main():
+    """Driver function, runs other necesssary fucntions."""
     googlenews = initalize_google_news()
     scrape_google_news_search(googlenews)
 
 
 def scrape_google_news_search(googlenews):
-    """Google News Web Scraping."""
+    """Scrapes a Google News web search using a specifc query."""
 
     googlenews.clear()  # clear past results
 
@@ -35,17 +36,22 @@ def scrape_google_news_search(googlenews):
 
 
 def initalize_google_news():
+    """Initializes the googlenews object."""
+
     print("initalize_google_news...")
-    # initialize google news:
-    googlenews = GoogleNews()
+
+    googlenews = GoogleNews()  # create googlenews object
     googlenews = GoogleNews(lang="en")
     googlenews = GoogleNews(period="d")
     googlenews = GoogleNews(encode="utf-8")
     googlenews = GoogleNews(start="09/01/2020", end="09/21/2020")
+
     return googlenews
 
 
 def scrape_article(link):
+    """Scrapes a specific article given a link."""
+
     article = Article(link)
     article.download()
     article.parse()
