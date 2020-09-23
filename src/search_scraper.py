86b8fb9f670cf0120cf00447a98acdef3b58d42b
Christian Lussier <lussierc@allegheny.edu>
2020-09-23 18:47:04 -0400

    Code formatting & printing improvements

--- src/search_scraper.py
+++ src/search_scraper.py
@@ -2,39 +2,48 @@
 
 from GoogleNews import GoogleNews
 from newspaper import Article
+
 # pip3 install GoogleNews, pip install newspaper3k
+
 
 def main():
     googlenews = initalize_google_news()
     scrape_google_news_search(googlenews)
 
+
 def scrape_google_news_search(googlenews):
     """Google News Web Scraping."""
+
     googlenews.clear()  # clear past results
 
-
     # set search query parameters:
-    googlenews = GoogleNews(start='09/01/2020',end='09/21/2020')
-    googlenews.search('apple site:https://www.wsj.com')
+    googlenews.search("apple site:https://www.wsj.com")
     googlenews.getpage(1)
 
     # print the results:
-    print(googlenews.result())  # prints all info
+    print("*** Gathering results:")
     results = googlenews.result()
-    print("\n\n\n")
-    print(googlenews.gettext())  # prints titles
-    print("\n\n\n")
+    print(results, "\n\n")  # prints all info
+
+    print("*** Gathering titles:")
+    titles = googlenews.gettext()
+    print(titles, "\n\n")  # prints titles
+
+    print("*** Printing results:")
     for result in results:
         print("\n***", result)
+
 
 def initalize_google_news():
     print("initalize_google_news...")
     # initialize google news:
     googlenews = GoogleNews()
-    googlenews = GoogleNews(lang='en')
-    googlenews = GoogleNews(period='d')
-    googlenews = GoogleNews(encode='utf-8')
+    googlenews = GoogleNews(lang="en")
+    googlenews = GoogleNews(period="d")
+    googlenews = GoogleNews(encode="utf-8")
+    googlenews = GoogleNews(start="09/01/2020", end="09/21/2020")
     return googlenews
+
 
 def scrape_article(link):
     article = Article(link)
@@ -43,5 +52,6 @@ def scrape_article(link):
 
     print("Article Text:")
     print(article.text)
+
 
 main()
