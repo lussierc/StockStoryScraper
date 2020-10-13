533a9e942b2d373998e16e273ce45cb788697cc5
Christian Lussier <lussierc@allegheny.edu>
2020-10-13 13:30:55 -0400

    Display key article attributes in Streamlit

--- src/interface.py
+++ src/interface.py
@@ -10,7 +10,20 @@ st.title("StockTextMining")
 data = main()
 print("\n\n\nDATADOGS:", data)
 
-for i in len(data):
-    st.subheader("Article #", i)
+for i in range(len(data)):
     article = data[i]
-    st.markdown("- Title:", article['title'])
+
+    st.markdown("## Article", i)
+
+    st.markdown("### Title:")
+    st.markdown(article['title'])
+    st.markdown("#### Title Sentiment:")
+    st.markdown(article['title_sent'])
+
+    st.markdown("### Description:")
+    st.markdown(article['desc'])
+    st.markdown("#### Description Sentiment:")
+    st.markdown(article['desc_sent'])
+
+    st.markdown("### Overall Text Sentiment:")
+    st.markdown(article['text_sent'])
