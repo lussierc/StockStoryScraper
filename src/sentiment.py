0f16a37b16baaf1c34ad570e98a849f5f4e2a3c0
Christian Lussier <lussierc@allegheny.edu>
2020-10-12 22:47:27 -0400

    Split sentiment.py up into crude temp functions

--- src/sentiment.py
+++ src/sentiment.py
@@ -14,8 +14,12 @@ from sklearn.metrics import confusion_matrix, classification_report, accuracy_sc
 from sklearn.svm import LinearSVC
 import string
 import en_core_web_sm
+from spacy.lang.en.stop_words import STOP_WORDS
 
 from search_scraper import *
+nlp = en_core_web_sm.load()
+punct = string.punctuation
+stopwords = list(STOP_WORDS)
 
 def dataCleaning(sentence):
     doc = nlp(sentence)
@@ -32,69 +36,68 @@ def dataCleaning(sentence):
             clean_tokens.append(token)
     return clean_tokens
 
-# load spacy small model as the nlp
-nlp = en_core_web_sm.load()
-from spacy.lang.en.stop_words import STOP_WORDS
+def main():
+    # load spacy small model as the nlp
 
-# gather stop words & punctutation
-stopwords = list(STOP_WORDS)
-punct = string.punctuation
+    # gather stop words & punctutation
 
-# TRAIN
-data_yelp = pd.read_csv("data/data.txt", sep="\t", header=None)  # currently using yelp review data
-columnName = ["Review", "Sentiment"]
-data_yelp.columns = columnName
+    # TRAIN
+    data_yelp = pd.read_csv("data/data.txt", sep="\t", header=None)  # currently using yelp review data
+    columnName = ["Review", "Sentiment"]
+    data_yelp.columns = columnName
 
-print(data_yelp.head(5))
-print(data_yelp.shape)
+    print(data_yelp.head(5))
+    print(data_yelp.shape)
 
-data = data_yelp
+    data = data_yelp
 
-X = data["Review"]
-y = data["Sentiment"]
-X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2)
-# print(X_train.shape, y_test.shape)
+    X = data["Review"]
+    y = data["Sentiment"]
+    X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2)
+    # print(X_train.shape, y_test.shape)
 
 
-# preparing the model:
-tfidf = TfidfVectorizer(tokenizer=dataCleaning)
-svm = LinearSVC()
-steps = [("tfidf", tfidf), ("svm", svm)]
-pipe = Pipeline(steps)
+    # preparing the model:
+    tfidf = TfidfVectorizer(tokenizer=dataCleaning)
+    svm = LinearSVC()
+    steps = [("tfidf", tfidf), ("svm", svm)]
+    pipe = Pipeline(steps)
 
-pipe.fit(X_train, y_train)
+    pipe.fit(X_train, y_train)
 
-y_pred = pipe.predict(X_test)
-# print(classification_report(y_test, y_pred))
-# print("\n\n")
-# print(confusion_matrix(y_test, y_pred))
+    y_pred = pipe.predict(X_test)
+    # print(classification_report(y_test, y_pred))
+    # print("\n\n")
+    # print(confusion_matrix(y_test, y_pred))
 
-articles = run()
-articles = articles[0] # remove outer list layer
+    articles = run()
+    articles = articles[0] # remove outer list layer
 
-for article in articles:
-    title = []
-    desc = []
-    text = []
+    for article in articles:
+        title = []
+        desc = []
+        text = []
 
-    print("\n\n------ ARTICLE: -------")
-    print("* Title:", article['title'])
-    title.append(article['title'])
-    title_sent = pipe.predict(title)
-    print("* * Title Sentiment Rating:", title_sent[0])
-    article['title_sent'] = title_sent[0]
+        print("\n\n------ ARTICLE: -------")
+        print("* Title:", article['title'])
+        title.append(article['title'])
+        title_sent = pipe.predict(title)
+        print("* * Title Sentiment Rating:", title_sent[0])
+        article['title_sent'] = title_sent[0]
 
-    print("* Desc:", article['desc'])
-    desc.append(article['desc'])
-    desc_sent = pipe.predict(desc)
-    print("* * Desc Sentiment Rating:", desc_sent[0])
-    article['desc_sent'] = desc_sent[0]
+        print("* Desc:", article['desc'])
+        desc.append(article['desc'])
+        desc_sent = pipe.predict(desc)
+        print("* * Desc Sentiment Rating:", desc_sent[0])
+        article['desc_sent'] = desc_sent[0]
 
-    print("* Text: (Keywords):",)
-    print(dataCleaning(article['text']))
-    text.append(article['text'])
-    text_sent = pipe.predict(text)
-    print("* * Text Sentiment Rating:", text_sent[0])
-    article['text_sent'] = text_sent[0]
+        print("* Text: (Keywords):",)
+        print(dataCleaning(article['text']))
+        text.append(article['text'])
+        text_sent = pipe.predict(text)
+        print("* * Text Sentiment Rating:", text_sent[0])
+        article['text_sent'] = text_sent[0]
 
-print(articles)
+    print(articles)
+    return articles
+main()
