c20d214aedad1b3ec63b7336d65c61878ca7e408
Christian Lussier <lussierc@allegheny.edu>
2020-11-05 02:16:46 -0500

    Create base file for csv_handler.py

---
+++ src/csv_handler.py
@@ -0,0 +1,12 @@
+"""Will handle output of data to CSV and handle input of old data to CSV. Will also verify that incoming data is new."""
+
+# output function
+
+# input function if user says they have an input file
+# read csv into dicts, modify if nc to meet the proper format
+# if user says they have an input file, bypass doing the stock names/sites
+# sends the date of last run & stocks/sites to search_scraper so new date range is ran
+# will call verify_new_data
+
+# verify_new_data()
+# which makes sure there are no duplicate article links
