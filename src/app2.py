2e6e874d7d4959de6a5341256bad468abc6feab1
Christian Lussier <lussierc@allegheny.edu>
2020-12-03 23:27:12 -0500

    Print out textual information for stock info gathered from previous article CSVs

--- src/app2.py
+++ src/app2.py
@@ -37,7 +37,6 @@ def app():
     scored_articles = articles  # since articles are only read in from csv, they are already scored
 
     for article in scored_articles:
-        st.write(article['title'])
         if article["stock"] in stocks_list:
             pass
         else:
@@ -61,10 +60,66 @@ def app():
 
     fin_scored_stocks = results_generator.generate_results(stocks_list, abbrv_list, scored_articles)
 
-    for stock in fin_scored_stocks:
-        st.write(stock['stock'])
-        st.write(stock['avg_stock_sent_score'])
+    st.markdown("## View Summary Info for all Stocks:")
+    if st.checkbox('See All Stocks Overview'):
+        df = pd.DataFrame(fin_scored_stocks, index = stocks_list).T
+        st.bar_chart(df)  # display dataframe/graph that vizualizes commit info
 
-    df = pd.DataFrame(fin_scored_stocks, index = stocks_list).T
+    #checkboxes for the price, checkbox for stock well being, checkbox for sents, checkbox for media
+
+    st.markdown('## See Individual Stock Graphs/Info')
+    for stock in fin_scored_stocks:
+
+        st.markdown("### View Content For Stock:")
+
+        if st.checkbox(stock['stock']):
+            st.markdown("### Price Information:")
+            st.markdown("#### Current Price at Time of Scrape:")
+            st.write(stock['current_price'])
+            st.markdown("#### Current Volume at Time of Scrape:")
+            st.write(stock['volume'])
+            st.markdown("#### Average Volume at Time of Scrape:")
+            st.write(stock['avg_volume'])
+
+            st.markdown("### Stock Well Being Prediction:")
+            st.write(stock['stock_well_being_prediction'])
+            st.markdown("#### The tool rates this stock as having this level of well being: " + stock['stock_well_being_prediction_feelings'])
+
+            if st.checkbox('View Sentiment Information'):
+                st.markdown('### Average Stock Sentiment Score:')
+                st.write(stock['avg_stock_sent_score'])
+                st.markdown('#### # of Articles:')
+                st.write(stock['article_count'])
+
+                st.write("### View Specific Sentiment Info:")
+                if st.checkbox('View Recent Sentiment Info'):
+                    # within this give the option for looking at the graph or textual
+                    st.markdown("### Recent Sentiment:")
+                    st.markdown("#### Within the last week:")
+                    st.write(stock['rcnt_text_sent_score'])
+                    st.markdown("Number of articles within this time frame:")
+                    st.write(stock['recent_article_count'])
+                    st.markdown("#### Within the last day:")
+                    st.write(stock['day_stock_sent_score'])
+                    st.markdown("Number of articles scraped/analyzed for the last day:")
+                    st.write(stock['day_article_count'])
+
+                if st.checkbox('View Media Specific Ratings'):
+                    for media in stock['media_results']:
+                        st.markdown("### Media Source: " + media['media'])
+
+                        st.markdown("#### Media Average Sent Score:")
+                        st.write(media['media_avg_sent_score'])
+
+                        st.markdown("For this media source, the sentiment was rated overall as:")
+                        st.write(media['media_sent_rating'])
+
+                        st.markdown("#### Article Count:")
+                        st.write(media['article_count'])
+                st.markdown("### Overall Stock Trifold Rating")
+                st.markdown("Average rating of all article sentiment ratings for the title, description/summary, and text.")
+                st.write(stock['ovr_stock_trifold_rating'])
+
+
+
     # columns = st.multiselect(label="Enter the names of specific contributors below:", options=df.columns)  # allow users to display specific contributor information on dataframe graph
-    st.bar_chart(df)  # display dataframe/graph that vizualizes commit info
--- src/web_app.py
+++ src/web_app.py
@@ -3,10 +3,10 @@ import app2
 
 import streamlit as st
 PAGES = {
-    "App1": app1,
-    "App2": app2
+    "Home": app1,
+    "Read in CSV": app2
 }
 st.sidebar.title('Navigation')
-selection = st.sidebar.radio("Go to", list(PAGES.keys()))
+selection = st.sidebar.radio("Go to:", list(PAGES.keys()))
 page = PAGES[selection]
 page.app()
